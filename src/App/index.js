597db6171bb7faab395a5955e0731eb6855dcb3f
crespoxx <32083272+crespoxx@users.noreply.github.com>
2021-01-19 22:37:02 -0300

    Update index.js

--- src/App/index.js
+++ src/App/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter as Router, Route  } from 'react-router-dom'
 import Screens from './Screens'
 export default function App() {
-
   return (
     <>
       <Router>
